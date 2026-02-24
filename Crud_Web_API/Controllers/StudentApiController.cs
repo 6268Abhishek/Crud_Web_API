68ab95e2c7ed970ccec0e9aa28374dba9acd65b9
626856abhishekraghuwanshi@gmail.com <626856abhishekraghuwanshi@gmail.com>
2026-02-24 14:03:12 +0530

    Add project files.

---
+++ Crud_Web_API/Controllers/StudentApiController.cs
@@ -0,0 +1,68 @@
+﻿using Crud_Web_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Crud_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentApiController : ControllerBase
+    {
+        private readonly CodeFirstDbContext context;
+        public StudentApiController(CodeFirstDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task< ActionResult<List<Student>>>GetStudents()
+        {
+            var data=await context.Students.ToListAsync();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>>GetStudentsById(int id)
+        {
+            var student=await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Student>>CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>>UpdateStudent(int id,Student std)
+        {
+            if(id!=std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(std);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>>DeleteData(int id)
+        {
+            var std = await context.Students.FindAsync(id);
+            if(std==null)
+            {
+                return NotFound();
+            }
+            context.Students.Remove(std);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
+  
---
+++ Crud_Web_API/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crud_Web_API.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Desgination { get; set; } = null!;
+
+    public int Salary { get; set; }
+}
